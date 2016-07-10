6aaf087b311f017a465e1ed0d86ec30d1b9edc4a
Look Wisely <denis.shtanko@gmail.com>
2016-07-10 14:25:26 +0300

    initial commit

---
+++ PlayerCharacterController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacterController : MonoBehaviour
+{
+
+
+    public GameObject ForegroundLayer2;
+    public GameObject ForegroundLayer1;
+    public GameObject BackgroundLayer1;
+    public GameObject BackgroundLayer2;
+    public GameObject BackgroundLayer3;
+    public GameObject BackgroundLayer4;
+    public GameObject BackgroundLayerSkybox;
+
+
+
+
+    // Use this for initialization
+    void Start () {
+	    
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ WorldRenderer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldRenderer : MonoBehaviour
+{
+    public Camera theCamera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
