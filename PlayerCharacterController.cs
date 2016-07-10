b122f7da608292ec7da562728b7ab8e61af1a71e
Look Wisely <denis.shtanko@gmail.com>
2016-07-11 00:30:49 +0300

    added analytics static method and character basic moving physics

--- PlayerCharacterController.cs
+++ PlayerCharacterController.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using Assets;
 
 public class PlayerCharacterController : MonoBehaviour
 {
@@ -32,16 +34,26 @@ public class PlayerCharacterController : MonoBehaviour
     private Vector2 _saved_position;
     private Vector2 _delta_buffer;
 
+    private Vector2 dir_up = new Vector2(0f, 1f);
+    private Vector2 dir_down = new Vector2(0f, -1f);
+    private Vector2 dir_left = new Vector2(-1f, 0f);
+    private Vector2 dir_right = new Vector2(1f, 0f);
 
+    private Dictionary<Vector2, bool> direction_stack;// = new Dictionary<Vector2, bool>();
+    public Rigidbody2D self;
 
+    public Camera theCamera;
+    [Range(0.3f, 3f)]
+    public float PlayerSpeed;
+    [Range(3f, 5f)]
+    public float PlayerAcceleration;
 
-    // Use this for initialization
     void Start ()
     {
         _saved_position = this.transform.position;
+        direction_stack = new Dictionary<Vector2, bool>();
     }
-	
-	// Update is called once per frame
+	//----------------------------------------------------------------------------------------
 	void Update ()
 	{
 	    _delta_buffer = (Vector2)this.transform.position - _saved_position;
@@ -58,5 +70,55 @@ public class PlayerCharacterController : MonoBehaviour
 
 
         }
+
+        mapWASD();
+	    _saved_position = this.transform.position;
 	}
+    //----------------------------------------------------------------------------------------
+
+
+    void mapWASD()
+    {
+
+        direction_stack[dir_left] = false;
+        direction_stack[dir_right] = false;
+        direction_stack[dir_up] = false;
+        direction_stack[dir_down] = false;
+
+        if (Input.GetKey("a"))
+            direction_stack[dir_left] = true;
+        else
+            direction_stack[dir_left] = false;
+        if (Input.GetKey("d"))
+            direction_stack[dir_right] = true;
+        else
+            direction_stack[dir_right] = false;
+
+        MovePlayerBody();
+
+    }
+
+    void MovePlayerBody()
+    {
+        var _sum_dir = new Vector2(0f, 0f);
+        foreach (Vector2 _key in direction_stack.Keys)
+            if (direction_stack[_key])
+                _sum_dir += _key;
+
+        var GoalVector = _sum_dir.normalized * PlayerSpeed;
+        var acceleration_direction = GoalVector - self.velocity;
+        acceleration_direction.y = 0f;
+
+        Debug.Log(acceleration_direction);
+
+        acceleration_direction.Normalize();
+
+        if (GAnalytics.EquifyDZ(GoalVector, self.velocity + (acceleration_direction * PlayerAcceleration), PlayerAcceleration) != self.velocity)
+        {
+            self.AddForce(acceleration_direction * PlayerAcceleration);
+        }
+        else
+            self.velocity = GoalVector;
+
+    }
 }
